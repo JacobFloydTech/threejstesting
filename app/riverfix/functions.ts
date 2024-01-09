9c72c87d4facf1f9c4d148dad7781e4fde085cba
jacob <jacobfl2013@gmail.com>
2024-01-09 19:14:39 +1300

    Update 9/1/24

--- app/riverfix/functions.ts
+++ app/riverfix/functions.ts
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
-import { Water } from 'three/examples/jsm/Addons.js';
+import {  TextGeometry, Water } from 'three/examples/jsm/Addons.js';
 import { OBJLoader } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 //@ts-ignore
 import { Noise } from 'noisejs'
 
@@ -36,16 +37,16 @@ export function addWater(scene: THREE.Scene) {
 }
 
 export function addPlane(scene: THREE.Scene) {
-
-    const geometry = new THREE.PlaneGeometry(100, 50, 50, 50, );
+    const geometry = new THREE.PlaneGeometry(100, 50, 120, 120,);
     const texture = new THREE.TextureLoader().load('/grass.jpg')
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide })
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(5, 5)
-    const planeMesh = new THREE.InstancedMesh(geometry, material, 20);
+    const planeMesh = new THREE.InstancedMesh(geometry, material, 10);
     const dummy = new THREE.Object3D();
     const offset = 100;
+    const position = new THREE.Vector3(0, 0, 0);
     for (var i = 0; i < planeMesh.count; i++) {
         let zOffset = -offset * (i - 1)
         dummy.position.set(0, 0, zOffset)
@@ -57,8 +58,21 @@ export function addPlane(scene: THREE.Scene) {
     planeMesh.instanceMatrix.needsUpdate = true;
     planeMesh.name = 'plane'
     scene.add(planeMesh)
-    addWater(scene)
-    loadObjGrassModel(scene);
+    addWater(scene);
+    addGrass(scene, planeMesh)
+    addMiddleGround(scene);
+    
+
+
+}
+
+function addGrass(scene: THREE.Scene, mesh: THREE.InstancedMesh) { 
+    const count = (mesh.geometry.attributes.position.array.length / 3) * 10;
+    const geometry = new THREE.BoxGeometry(Math.random()*0.7,1.2,Math.random()*0.7); 
+    const boxMesh = new THREE.InstancedMesh(geometry, leavesMaterial, count);
+    boxMesh.name = 'grass'
+    boxMesh.rotation.set(Math.PI / 2, 0, Math.PI / 2)
+    setPositions(scene, boxMesh, mesh.geometry.attributes.position.array);
 
 }
 
@@ -74,72 +88,321 @@ function updatePoints(points: THREE.TypedArray, mesh: THREE.InstancedMesh) {
         y *= divide;
         const percentageToCenter = (1 - Math.abs(x) / 25);
 
-        const startingPoint = -5 + (percentageToCenter * 10);
+        const startingPoint = -8 + (percentageToCenter * 10);
 
-        if (y > startingPoint && y < startingPoint + 10) {
-            points[i + 2] += 10;
+        if (y > startingPoint && y < startingPoint + 15) {
+            points[i + 2] += 100;
 
         }
 
-        if (y == startingPoint || y == startingPoint + 10) {
+        if (y == startingPoint || y == startingPoint + 15) {
             points[i + 2] += 4;
         }
     }
     mesh.instanceMatrix.needsUpdate = true;
+}   
+
+export function addMiddleGround(scene: THREE.Scene) { 
+    const geo = new THREE.PlaneGeometry(400, 100, 100, 100);
+    const texture = new THREE.TextureLoader().load('/grass.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide })
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(5, 5)
+    const mesh = new THREE.InstancedMesh(geo, material, 1); 
+    mesh.rotation.set(Math.PI / 2, 0, Math.PI); 
+    mesh.position.y = -5;
+    const dummy = new THREE.Object3D();
+    for (var i = 0; i < mesh.count; i++) { 
+        dummy.position.y = 100 * (i - 1);
+        dummy.updateMatrix();
+        mesh.setMatrixAt(i, dummy.matrix);
+    }
+    const points = mesh.geometry.attributes.position.array;
+    const divide = 12;
+    const noise = new Noise();
+    for (var i = 0; i < points.length; i += 3) { 
+        const x = points[i] / divide;
+        const y = points[i + 1] / divide;
+        const z = points[i + 2] / divide;
+        points[i + 2] = noise.perlin3(x, y, z) * 4;
+    }
+    mesh.name = 'middle'
+    mesh.instanceMatrix.needsUpdate = true;
 
 
+    scene.add(mesh)
 }
 
-export function loadObjGrassModel(scene: THREE.Scene) { 
-    const loader = new GLTFLoader();
 
-
-    loader.load('/grass.glb', (gltf) => {
-        const e = gltf.scene.children[0].children[1];
-        const points = (scene.getObjectByName('plane') as THREE.InstancedMesh).geometry.attributes.position.array;
-        const childMesh = new THREE.InstancedMesh((e as THREE.Mesh).geometry, (e as THREE.Mesh).material, (points.length / 3) * 10);
-        childMesh.rotation.set(Math.PI / 2, 0, Math.PI / 2)
-        setPositions(scene, childMesh, points);
-    })
-   
-}   
 
 function setPositions(scene: THREE.Scene, mesh: THREE.InstancedMesh, points: THREE.TypedArray) { 
     const dummy = new THREE.Object3D();
+   
+
     for (var instances = 0; instances <= 10; instances++) { 
         for (var i = 0; i < points.length; i += 3) { 
-            if (Math.random() > 0.8) { 
-                const x = Math.random()*-1500;
+                const x = points[i] + (instances * -100) +  getRandomArbitrary(-1, 1)
                 const y = points[i + 1] + getRandomArbitrary(-1, 1)
-                const percentageToCenter = (1-Math.abs(x)/25);
-                const startingPoint = -5+(percentageToCenter*10);
-                let z;
-                if (y > startingPoint && y < startingPoint + 10) { 
-                    z = 100;
-                } else { 
-                    z = points[i + 2] -2;
-                }
-         
-                const scale = getRandomArbitrary(1, 2);
+                const z = points[i + 2];
+                dummy.rotation.set(Math.PI / 2, 0, Math.PI)
+                
                 dummy.position.set(x, y, z);
-                dummy.scale.set(scale, scale, scale);
+               
+             
                 dummy.updateMatrix();
+
+        
                 mesh.setMatrixAt(i / 3 + (instances * (points.length / 3)), dummy.matrix);
-            
-
-            }
-              
-            
-
+           
         }
     }
+    mesh.name = 'grass'
     mesh.instanceMatrix.needsUpdate = true;
     scene.add(mesh);
 }
 
+export function addText(scene: THREE.Scene, text: string) { 
+    const loader = new FontLoader();
+    loader.load('/font.json', (font) => { 
+        const textGeo = new TextGeometry(text, {
+            font: font,
+            size: 5,
+            height: 0,  
+        })
+     
+        const textMesh = new THREE.Mesh(textGeo, textMaterial)
+        textMesh.name = text;
+        textMesh.position.set(-30, 10, -400);
+        scene.add(textMesh);
+    })
+    
+}
 
 
+export function animateText(starting: number, ending: number, current: number, id: string, scene: THREE.Scene) { 
+    const obj = scene.getObjectByName(id);
+    if (!obj) { return }
+    let mesh = scene.getObjectByName(id) as THREE.Mesh;
+    let materials: THREE.Material | THREE.Material[] = (obj as THREE.Mesh).material;
+   
+    if (!Array.isArray(materials)) {
+        materials = [materials];
+    }
+    materials.forEach((m) => { 
+        let per = (current - starting) / (ending - starting) ;
+        per = Math.min(1, Math.max(0, per));
+        (m as THREE.ShaderMaterial).uniforms.opacity.value = per;
+        m.needsUpdate = true
+        mesh.position.y = per * 10 + 10;
+    })
+
+}
+
+export function addBackgroundLandscape() { 
+   const geometry = new THREE.BoxGeometry(600, 1, 100, 200, 10, 200);
+    let mesh = new THREE.InstancedMesh(geometry,new  THREE.MeshLambertMaterial({color: 0x3c3951}), 10);
+    let dummy = new THREE.Object3D();
+    for (var i = 0; i < mesh.count; i++) {
+        dummy.position.z = -100 * (i - 1);
+        dummy.position.y = -10;
+        dummy.updateMatrix();
+        mesh.setMatrixAt(i, dummy.matrix)
+
+    }
+    addBackgroundNoise(mesh);
+    return mesh;
+
+}
+
+function addBackgroundNoise(mesh: THREE.InstancedMesh) { 
+    const noise = new Noise();
+    const divide = 50;
+    const points = mesh.geometry.attributes.position.array;
+    for (var i = 0; i < points.length; i += 3) { 
+        let x = points[i]/divide;
+        const y = points[i + 1]/divide;
+        const z = points[i + 2] / divide;
+        const noiseValue = noise.simplex2(x, z);
+        const distanceToCenter = Math.abs(x);
+        const amp = 1 - distanceToCenter ** 3.2;
+        points[i + 1] += noiseValue * amp;
+    }
+    mesh.instanceMatrix.needsUpdate = true;
+  
+}
+
+
+export function createVideoElement() {
+    const video = document.createElement('video');
+    video.muted = true;
+    video.loop = true;
+    video.src = '/h264test.webm'
+    video.load();
+    video.play()
+    const videoTexture = new THREE.VideoTexture(video);
+    videoTexture.minFilter = THREE.LinearFilter;
+    videoTexture.magFilter = THREE.LinearFilter;
+    const material = new THREE.MeshBasicMaterial({ map: videoTexture });
+    const geometry = new THREE.PlaneGeometry(16, 9); // Adjust size as needed
+
+    // Create a mesh with the video material and geometry
+    const mesh = new THREE.Mesh(geometry, material);    
+    mesh.position.set(0, 20,  -20);
+    return mesh
+
+
+}
 
 function getRandomArbitrary(min:number, max: number) {
     return Math.random() * (max - min) + min;
 }
+
+
+
+// Create a custom shader material
+const textMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        opacity: { value: 0 }, // Set your initial opacity value
+    },
+    vertexShader: `
+        precision highp float;
+        varying vec2 vUv;
+        
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        precision highp float;
+        varying vec2 vUv;
+        uniform float opacity;
+        
+        void main() {
+            // Gradient effect from yellow to gold
+            vec3 gradientColor = mix(vec3(1.0, 1.0, 0.0), vec3(0.97, 0.843, 0.0), vUv.y);
+            
+            // Combine with opacity
+            vec4 finalColor = vec4(gradientColor, opacity);
+            
+            gl_FragColor = finalColor;
+        }
+    `,
+    transparent: true, // Enable transparency in the material
+});
+
+
+
+let simpleNoise = `
+    float N (vec2 st) { // https://thebookofshaders.com/10/
+        return fract( sin( dot( st.xy, vec2(12.9898,78.233 ) ) ) *  43758.5453123);
+    }
+    
+    float smoothNoise( vec2 ip ){ // https://www.youtube.com/watch?v=zXsWftRdsvU
+    	vec2 lv = fract( ip );
+      vec2 id = floor( ip );
+      
+      lv = lv * lv * ( 3. - 2. * lv );
+      
+      float bl = N( id );
+      float br = N( id + vec2( 1, 0 ));
+      float b = mix( bl, br, lv.x );
+      
+      float tl = N( id + vec2( 0, 1 ));
+      float tr = N( id + vec2( 1, 1 ));
+      float t = mix( tl, tr, lv.x );
+      
+      return mix( b, t, lv.y );
+    }
+  `;
+
+
+const vertexShader = `
+  varying vec2 vUv;
+  uniform float time;
+  
+  ${simpleNoise}
+  
+	void main() {
+
+    vUv = uv;
+    float t = time * 2.;
+    
+    // VERTEX POSITION
+    
+    vec4 mvPosition = vec4( position, 1.0 );
+    #ifdef USE_INSTANCING
+    	mvPosition = instanceMatrix * mvPosition;
+    #endif
+    
+    // DISPLACEMENT
+    
+    float noise = smoothNoise(mvPosition.xz * 0.5 + vec2(0., t));
+    noise = pow(noise * 0.5 + 0.5, 2.) * 3.;
+    
+    // here the displacement is made stronger on the blades tips.
+    float dispPower = 1. - cos( uv.y * 3.1416 * 0.5 );
+    
+    float displacement = noise * ( 0.3 * dispPower );
+    mvPosition.z -= displacement;
+    
+    //
+    
+    vec4 modelViewPosition = modelViewMatrix * mvPosition;
+    gl_Position = projectionMatrix * modelViewPosition;
+
+	}
+`;
+
+const fragmentShader = `
+  varying vec2 vUv;
+  
+  void main() {
+  	vec3 baseColor = vec3( 0.41, 1.0, 0.3 );
+    float clarity = ( vUv.y * 0.875 ) + 0.125;
+    gl_FragColor = vec4( baseColor * clarity, 1 );
+  }
+`;
+
+const uniforms = {
+	time: {
+  	value: 0
+  }
+}
+
+const leavesMaterial = new THREE.ShaderMaterial({
+	vertexShader,
+  fragmentShader,
+  uniforms,
+  side: THREE.DoubleSide
+});
+
+
+
+
+
+export const scaleVertexShader = `
+        varying vec2 vUv;
+        uniform vec2 aspect;
+
+        void main() {
+            vUv = uv;
+            vec3 newPosition = position;
+            
+            // Scale up each vertex based on camera distance
+            float distance = length(newPosition);
+            float scaleFactor = 1.  +  distance * 0.1; // Adjust the scaling factor as needed
+            newPosition *= scaleFactor;
+
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+        }`
+
+export const scaleFragShader = `        
+        varying vec2 vUv;
+        uniform sampler2D tDiffuse;
+
+        void main() {
+            gl_FragColor = texture2D(tDiffuse, vUv);
+        }`
+
--- app/riverfix/page.tsx
+++ app/riverfix/page.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { addPlane, addWater, loadObjGrassModel } from './functions';
+import { addBackgroundLandscape, addMiddleGround, addPlane, addText, addWater, animateText, createVideoElement, scaleFragShader, scaleVertexShader } from './functions';
 import { MutableRefObject, useEffect, useRef } from "react"
 import * as THREE from 'three'
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { EffectComposer, OrbitControls, RenderPass, ShaderPass } from "three/examples/jsm/Addons.js";
+import { Water } from 'three/examples/jsm/Addons.js';
 import { RGBELoader } from "three/examples/jsm/Addons.js";
 
 
@@ -18,41 +19,66 @@ export default function Page() {
 function setScene(ref: MutableRefObject<any>) {
     if (!ref.current) { return }
     let velocity = 0;
-
-
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 300);
+    const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     ref.current.appendChild(renderer.domElement);
-
+    let text = "Risos Enterprises LTD"
     scene.add(new THREE.AmbientLight());
-    addPlane(scene)
-    const loader = new RGBELoader();
-    loader.load('/background.hdr', (texture) => {
-        scene.background = texture
-    })
-    camera.position.set(0, 20, 100);
+    addPlane(scene);
+
+    let material: THREE.Material | THREE.Material[] = (scene.getObjectByName('grass') as THREE.InstancedMesh)?.material;
+    const water = scene.getObjectByName('waterMesh') as Water;
+
+    camera.position.set(0, 15, 100);
+    camera.lookAt(0, 15, 50);
+
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+    const cameraShader = {
+        uniforms: {
+            tDiffuse: { value: null },
+            aspect: { value: new THREE.Vector2 }
+        }, vertexShader: scaleVertexShader, fragmentShader: scaleFragShader
+    }
+    const globeEffectPass = new ShaderPass(cameraShader);
+    globeEffectPass.renderToScreen = true;
+    composer.addPass(globeEffectPass);
 
     function animate() {
+        animateText(-5, -200, camera.position.z, text, scene);
+        changeScale(camera.position, scene)
 
 
         requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-        camera.position.z += velocity;
-        if (velocity > 0) { 
-            velocity -= 1;
-        } 
-        if (velocity < 0) { 
-            velocity += 1;
+        camera.position.z += velocity
+        if (velocity < 0) {
+            velocity += 0.025;
+        } else if (velocity > 0) {
+            velocity -= 0.025;
         }
-        if (camera.position.z < -600) { 
+        if (camera.position.z < -600) {
             camera.position.z = 0;
         }
 
+        if (material) {
+            if (!Array.isArray(material)) {
+                material = [material]
+            }
+            material.forEach((x) => {
+                (x as THREE.ShaderMaterial).uniforms.time.value += 0.01;
+                x.needsUpdate = true;
+            })
+        }
+        if (water) {
+            water.material.uniforms.time.value += 0.01
+
+        }
+
+        renderer.render(scene, camera)
     }
-
-
     function onWindowResize() {
 
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -62,20 +88,38 @@ function setScene(ref: MutableRefObject<any>) {
 
     }
     function onWheel(e: WheelEvent) {
-        velocity += e.deltaY*0.05;
+
+        velocity += roundToNearestIncrement(Math.round(e.deltaY), 0.05) * 0.001;
+
     }
     window.addEventListener('resize', onWindowResize, false);
     window.addEventListener('wheel', onWheel, false);
 
     animate()
 }
+function roundToNearestIncrement(number: number, increment: number) {
+    return Math.floor(number / increment) * increment;
+}
 
-function checkChildren(scene: THREE.Scene, position: number) {
-    scene.children.forEach((x) => {
-        if (x.position.z > position) {
-            setTimeout(() => {
-                scene.remove(x)
-            }, 1000);
-        }
-    })
+function changeScale(position: THREE.Vector3, scene: THREE.Scene) {
+    const mesh = scene.getObjectByName('grass') as THREE.InstancedMesh;
+    let dummy = new THREE.Object3D()
+    const minDistance = 0;
+    const maxDistance = 100;
+    console.log(position);
+
+    for (var i = 0; i < mesh.count; i++) {
+        let matrix = new THREE.Matrix4();
+        mesh.getMatrixAt(i, matrix);
+        dummy.position.setFromMatrixPosition(matrix);
+        dummy.setRotationFromMatrix(matrix)
+        dummy.updateMatrix()
+        const distance = dummy.position.distanceTo(position)
+        let per = Math.min(1, Math.max(0, maxDistance / distance));
+
+        dummy.scale.set(per, per, per);
+        dummy.updateMatrix()
+        mesh.setMatrixAt(i, dummy.matrix)
+    }
+    mesh.instanceMatrix.needsUpdate = true;
 }
